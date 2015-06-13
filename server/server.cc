e5e593f2b282f9729f6021c717abc50cc4fe5f55
zhehaowang <wangzhehao410305@gmail.com>
2015-06-12 17:16:47 -0700

    server: increase process events interval to 1ms

--- server/server.cc
+++ server/server.cc
@@ -156,7 +156,7 @@ int main(int argc, char **argv) {
   face.registerPrefix(prefix_name, ::onInterest, ::onRegisterFailed);
   while (true) {
 	face.processEvents();
-	usleep (20);
+	usleep (1000);
   }
 
   FILE_LOG(LOG_DEBUG) << "main: server exit." << endl;
